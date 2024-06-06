f367284cf158e21da5a1f83334416bd916b76516
ErikBouvier <pablobb.prog@gmail.com>
2024-06-06 00:00:30 -0300

    MUCHOS CAMBIOS CON MUCHAS MEJORAS

--- proyecto2.py
+++ proyecto2.py
@@ -2,63 +2,82 @@
 
 usuarios = {}
 
+# def para vlaidar contraseña
+
+
+def validar_contraseña(usuario, contrasena):
+    if usuarios[usuario] == contrasena:
+        return True
+    else:
+        return False
+
+
+# def para buscar un usuario particular
+
+
+def buscar_usuario(usuario):
+    if usuario.lower in usuarios:
+        return True
+    else:
+        return False
+
 # def para registrar usuarios
 
 
-def registro_usuario():
-    usuario = input("Ingrese el nombre de usuario:  ")
-    if usuario in usuarios:
-        print("El nombre de usuario ya existe.")
-        return
-    print("Usuario ingresado correctamente. \n")
-    contraseña = input("Ingrese su contraseña:  ")
-    usuarios[usuario] = contraseña
-    print("Contraseña ingresada correctamente. \n")
+def registro_usuario(usuario):
+    try:
+        if len(usuario) <= 0:
+            print("Por favor ingrese un dato valido: ")
+            return
+        if buscar_usuario(usuario):
+            print("El nombre de usuario ya existe.")
+            return
+        print("Usuario ingresado correctamente. ")
+        contraseña = input("Ingrese su contraseña: ")
+        usuarios[usuario] = contraseña
+        print("Contraseña ingresada correctamente.\n")
+    except Exception as e:
+        print(f"Error inesperado {e}")
 
 # def para borrar un usuario especifico con solicitud de contraseña
 
-# HEE QUE TE PASA
 
-
-def borrar_usuario():
+def borrar_usuario(usuario):
     if not usuarios:
         print("Aún no hay usuarios registrados.")
-    usuario = input("Ingrese el nombre de usuario a borrar: \n")
-    if usuario not in usuarios:
+    if buscar_usuario(usuario):
         print("El usuario no está registrado.")
         return
-    contraseña = input("Ingrese la contraseña del usuario: \n")
-    if usuarios[usuario] == contraseña:
+    contraseña = input("Ingrese la contraseña del usuario: ")
+    if validar_contraseña(usuario, contraseña):
         del usuarios[usuario]
-        print(f"El usuario {usuario} ha sido borrado con exito.")
+        print(f"El usuario {usuario} ha sido borrado con exito.\n")
     else:
-        print("Contraseña incorrecta. No se pudo borrar el usuario.")
+        print("Contraseña incorrecta. No se pudo borrar el usuario.\n")
 
 
 # def para mostrar usuarios
 
-def mostrar_usuario():
-    if not usuarios:
-        print("No hay usuarios registrados \n")
+def mostrar_usuario(usuario):
+    if usuario in usuarios:
+        print(f"Usuario: {usuario} -> Contraseña: {usuarios[usuario]}")
         return
-    for usuario, contraseña in usuarios.items():
-        print(f"Usuario: {usuario} -> Contraseña: {contraseña}")
+
 
 
 # def para loguearse
 
-def login():
+def login(usuario):
     while True:
-        usuario = input("Ingrese su nombre de usuario: \n")
-        if usuario not in usuarios:
-            print("El usuario no esta registrado.")
+        if buscar_usuario(usuario):
+            print("El usuario no esta registrado.\n")
             return
-        contraseña = input("Ingrese su contraseña: \n")
-        if usuarios[usuario] == contraseña:
-            print("Login exitoso.")
+        contraseña = input("Ingrese su contraseña: ")
+        if validar_contraseña(usuario, contraseña):
+            print("Login exitoso.\n")
             return usuario
         else:
-            print("Contraseña incorrecta. Intentelo nuevamente.")
+            print("Contraseña incorrecta. Intentelo nuevamente.\n")
             break
 
 
@@ -68,20 +87,28 @@ def login():
 def menu():
     while True:
         print("Menú: ")
-        print("1 - Registre un usuario")
+        print("1 - Registrar usuario")
         print("2 - Borrar usuario")
-        print("3 - Mostrar usuarios ")
-        print("4 - Login")
+        print("3 - Buscar usuario")
+        print("4 - Mostrar usuarios ")
+        print("5 - Login")
         print("6 - Salir")
-        opcion = input("Ingrese una opción: \n")
+        opcion = input("Ingrese una opción: ")
         if opcion == "1":
-            registro_usuario()
+            usuario = input("Ingrese el nombre de usuario: ")
+            registro_usuario(usuario)
         elif opcion == "2":
-            borrar_usuario()
+            usuario = input("Ingrese el nombre de usuario a borrar: ")
+            borrar_usuario(usuario)
         elif opcion == "3":
-            mostrar_usuario()
+            usuario = input("Ingrese el nombre de usuario que desea buscar: ")
+            mostrar_usuario(usuario)
         elif opcion == "4":
-            login()
+            for usuario in usuarios:
+                mostrar_usuario(usuario)
+        elif opcion == "5":
+            usuario = input("Ingrese su nombre de usuario: ")
+            login(usuario)
         elif opcion == "6":
             print("Saliendo del programa")
             break
@@ -93,3 +120,4 @@ def menu():
 
 
 menu()
+
